7566f8a201175831785df18aad482b7131ab527b
Sahib Jabbal <sahibjabbal@hotmail.com>
2023-04-02 10:58:56 +0300

    Initial progress of the application
    Created 2 separate virtual environments server and sensor and added the relevant files into it.

---
+++ sensor/unknown_sensor.py
@@ -0,0 +1,93 @@
+import socket
+import json
+import ssl
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+def get_temperature():
+    while True:
+        try:
+            temp = float(input("Enter temperature value: "))
+            return temp
+        except ValueError:
+            print("Invalid temperature value. Please try again.")
+
+def run_client():
+
+    # Sensor Type
+    sensor_type = "unknown_sensor" 
+
+    # create a socket object
+    s = socket.socket()
+
+    # get local machine name
+    host = socket.gethostname()
+
+    # define the port on which you want to connect
+    port = 12345
+
+    # connect to the server on local computer
+    context = ssl.create_default_context()
+    # Bypass SSL certificate verification
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    s = context.wrap_socket(s, server_hostname=host)
+    s.connect((host, port))
+
+    # define the sensor data
+    temperature = get_temperature()
+    sensor_data = {
+        "sensor_type": sensor_type,
+        "sensor_name": "temperature_sensor",
+        "sensor_value": temperature,
+        "sensor_unit": "Celsius",
+        "sensor_id": "xyz987" # new: add sensor id
+    }
+
+    # convert the sensor data to JSON format
+    sensor_json = json.dumps(sensor_data)
+
+    # set up the encryption key and IV
+    key = b'mysecretpassword'
+    iv = key
+
+    # ensure the IV is 16 bytes long
+    if len(iv) != 16:
+        raise ValueError("IV must be 16 bytes long")
+
+    # create the AES cipher object
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    # pad the sensor data to a multiple of 16 bytes
+    padded_data = pad(sensor_json.encode(), AES.block_size)
+
+    # encrypt the padded sensor data using AES-CBC
+    encrypted_data = cipher.encrypt(padded_data)
+
+    # send the encrypted sensor data to the server
+    s.sendall(encrypted_data)
+
+    # receive the response from the server
+    encrypted_response = s.recv(1024)
+    print("Received encrypted server response: ", encrypted_response)
+    
+    # create a new AES cipher object for decrypting the response data
+    decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    # decrypt the response using AES-CBC
+    decrypted_response = decrypt_cipher.decrypt(encrypted_response)
+
+    # unpad the decrypted response
+    unpadded_response = unpad(decrypted_response, AES.block_size)
+    
+    # convert the response from bytes to string format
+    response = unpadded_response.decode()
+
+    print("Decrypted Response from server: ", response)
+
+    # close the connection
+    s.close()
+
+run_client()
+
---
+++ server/server.py
@@ -0,0 +1,82 @@
+import socket
+import json
+import ssl
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from termcolor import colored
+
+def run_server():
+    # create a socket object
+    s = socket.socket()
+
+    # get local machine name
+    host = socket.gethostname()
+    
+    # define the port on which you want to bind
+    port = 12345
+    
+    # bind the socket to a public host and port
+    s.bind((host, port))
+    
+    # listen for incoming connections
+    s.listen(1)
+    print("Server is running on:", socket.gethostbyname(host), "port", port)
+    
+    # define the registered sensor IDs
+    registered_sensors = ["abc123", "def456", "ghi789"] 
+    
+    # define the shared secret key for message encryption
+    key = b'mysecretpassword'
+    iv = key
+    
+    while True:
+        # accept the incoming connection
+        client_socket, addr = s.accept()
+        print("Got a connection from ", addr)
+    
+        # wrap the socket in SSL/TLS
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+        client_socket = context.wrap_socket(client_socket, server_side=True)
+    
+        # receive the encrypted sensor data from the client
+        encrypted_sensor_data = client_socket.recv(1024)
+        print("Received encrypted sensor data: ", encrypted_sensor_data)
+    
+        # decrypt the sensor data using AES-CBC encryption
+        decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+        sensor_data = unpad(decrypt_cipher.decrypt(encrypted_sensor_data), AES.block_size).decode()
+        print("Decrypted sensor data: ", sensor_data)
+    
+        # parse the sensor data from JSON format
+        sensor_json = json.loads(sensor_data)
+    
+        # check if the sensor is valid
+        if sensor_json["sensor_type"] != "known_sensor":
+            response = colored("Unknown sensor", "red", "on_yellow", ["bold", "underline"])
+        else:
+            # check if sensor is registered
+            if sensor_json["sensor_id"] not in registered_sensors:
+                response = colored("Sensor not registered", "blue")
+            else:
+                # check the sensor data and send the appropriate response
+                if sensor_json["sensor_value"] > 30.0:
+                    response = colored("Turn on the AC", "yellow")
+                elif sensor_json["sensor_value"] < 10.0:
+                    response = colored("Turn on the heater", "yellow")
+                else:
+                    response = colored("Temperature is normal", "green")
+        print("Response to sensor:", response)
+
+        # encrypt the server response using AES-CBC encryption
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        padded_response = pad(response.encode(), AES.block_size)
+        encrypted_response = cipher.encrypt(padded_response)
+    
+        # send the encrypted response to the client
+        client_socket.send(encrypted_response)
+    
+        # close the connection
+        client_socket.close()
+
+run_server()
