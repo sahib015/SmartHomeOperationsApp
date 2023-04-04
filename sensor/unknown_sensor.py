efc61b8e34b4e3d3ede0f12a07f2bf95780b0bb0
kwyau126 <77718110+kwyau126@users.noreply.github.com>
2023-04-04 21:49:51 +0900

    Add files via upload

--- sensor/unknown_sensor.py
+++ sensor/unknown_sensor.py
@@ -3,91 +3,118 @@ import json
 import ssl
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+import random
+import time
 
 def get_temperature():
     while True:
         try:
-            temp = float(input("Enter temperature value: "))
+            # Get Temperature
+            temp = random.randint(1,40) 
             return temp
         except ValueError:
             print("Invalid temperature value. Please try again.")
+def getHumidity():
+    while True:
+        try:
+            #Get Humidity
+            humidity = random.randint(1,40)
+            return humidity
+        except ValueError:
+            print("Invalid Humidity value. Please try again.")
 
 def run_client():
 
-    # Sensor Type
-    sensor_type = "unknown_sensor" 
+    while True:
+        try:
+            # Set DSCP Value = AF41
+            DSCP = 0x90
+            
+            # Sensor Type
+            sensor_type = "unknown_sensor" 
 
-    # create a socket object
-    s = socket.socket()
+            # create a socket object
+            s = socket.socket()
 
-    # get local machine name
-    host = socket.gethostname()
+            # set the TOS marking for the socket
+            s.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, DSCP)
 
-    # define the port on which you want to connect
-    port = 12345
+            # get local machine name
+            host = socket.gethostname()
 
-    # connect to the server on local computer
-    context = ssl.create_default_context()
-    # Bypass SSL certificate verification
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+            # define the port on which you want to connect
+            port = 12345
 
-    s = context.wrap_socket(s, server_hostname=host)
-    s.connect((host, port))
+            # connect to the server on local computer
+            context = ssl.create_default_context()
+            # Bypass SSL certificate verification
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
 
-    # define the sensor data
-    temperature = get_temperature()
-    sensor_data = {
-        "sensor_type": sensor_type,
-        "sensor_name": "temperature_sensor",
-        "sensor_value": temperature,
-        "sensor_unit": "Celsius",
-        "sensor_id": "xyz987" # new: add sensor id
-    }
+            s = context.wrap_socket(s, server_hostname=host)
+            s.connect((host, port))
 
-    # convert the sensor data to JSON format
-    sensor_json = json.dumps(sensor_data)
+            # define the sensor data
+            temperature = get_temperature()
+            humidity = getHumidity()
+            sensor_data = {
+                "sensor_type": sensor_type,
+                "sensor_name": "temperatureHumidity_sensor",
+                "sensor_tempValue": temperature,
+                "sensor_humidityValue": humidity,
+                "sensor_unit": "Celsius and %",
+                "sensor_id": "xyz987" # new: add sensor id
+            }
 
-    # set up the encryption key and IV
-    key = b'mysecretpassword'
-    iv = key
+            # convert the sensor data to JSON format
+            sensor_json = json.dumps(sensor_data)
 
-    # ensure the IV is 16 bytes long
-    if len(iv) != 16:
-        raise ValueError("IV must be 16 bytes long")
+            # set up the encryption key and IV
+            key = b'mysecretpassword'
+            iv = key
 
-    # create the AES cipher object
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+            # ensure the IV is 16 bytes long
+            if len(iv) != 16:
+                raise ValueError("IV must be 16 bytes long")
 
-    # pad the sensor data to a multiple of 16 bytes
-    padded_data = pad(sensor_json.encode(), AES.block_size)
+            # create the AES cipher object
+            cipher = AES.new(key, AES.MODE_CBC, iv)
 
-    # encrypt the padded sensor data using AES-CBC
-    encrypted_data = cipher.encrypt(padded_data)
+            # pad the sensor data to a multiple of 16 bytes
+            padded_data = pad(sensor_json.encode(), AES.block_size)
 
-    # send the encrypted sensor data to the server
-    s.sendall(encrypted_data)
+            # encrypt the padded sensor data using AES-CBC
+            encrypted_data = cipher.encrypt(padded_data)
 
-    # receive the response from the server
-    encrypted_response = s.recv(1024)
-    print("Received encrypted server response: ", encrypted_response)
+            # send the encrypted sensor data to the server
+            s.sendall(encrypted_data)
+
+            # receive the response from the server
+            encrypted_response = s.recv(1024)
+            print("Received encrypted server response: ", encrypted_response)
     
-    # create a new AES cipher object for decrypting the response data
-    decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+            # create a new AES cipher object for decrypting the response data
+            decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
 
-    # decrypt the response using AES-CBC
-    decrypted_response = decrypt_cipher.decrypt(encrypted_response)
-
-    # unpad the decrypted response
-    unpadded_response = unpad(decrypted_response, AES.block_size)
+            # decrypt the response using AES-CBC
+            decrypted_response = decrypt_cipher.decrypt(encrypted_response)
     
-    # convert the response from bytes to string format
-    response = unpadded_response.decode()
+            # unpad the decrypted response
+            unpadded_response = unpad(decrypted_response, AES.block_size)
+    
+            # convert the response from bytes to string format
+            response = unpadded_response.decode()
 
-    print("Decrypted Response from server: ", response)
+            print("Decrypted Response from server: ", response)
 
-    # close the connection
-    s.close()
+            # close the connection
+            s.close()
+               
+            # Interval for the update
+            time.sleep(5)
 
+        except socket.error as e:
+            print(f"Socket error occurred: {e}")
+            
 run_client()
 
--- sensor/unreg_sensor.py
+++ sensor/unreg_sensor.py
@@ -3,91 +3,117 @@ import json
 import ssl
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+import random
+import time
 
 def get_temperature():
     while True:
         try:
-            temp = float(input("Enter temperature value: "))
+            # Get Temperature
+            temp = random.randint(1,40) 
             return temp
         except ValueError:
             print("Invalid temperature value. Please try again.")
+def getHumidity():
+    while True:
+        try:
+            #Get Humidity
+            humidity = random.randint(1,40)
+            return humidity
+        except ValueError:
+            print("Invalid Humidity value. Please try again.")
 
 def run_client():
+    while True:
+        try:
+            # Set DSCP Value = AF41
+            DSCP = 0x90
+            
+            # Sensor Type
+            sensor_type = "known_sensor" 
 
-    # Sensor Type
-    sensor_type = "known_sensor" 
+            # create a socket object
+            s = socket.socket()
 
-    # create a socket object
-    s = socket.socket()
+            # set the TOS marking for the socket
+            s.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, DSCP)
 
-    # get local machine name
-    host = socket.gethostname()
+            # get local machine name
+            host = socket.gethostname()
 
-    # define the port on which you want to connect
-    port = 12345
+            # define the port on which you want to connect
+            port = 12345
 
-    # connect to the server on local computer
-    context = ssl.create_default_context()
-    # Bypass SSL certificate verification
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+            # connect to the server on local computer
+            context = ssl.create_default_context()
+            # Bypass SSL certificate verification
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
 
-    s = context.wrap_socket(s, server_hostname=host)
-    s.connect((host, port))
+            s = context.wrap_socket(s, server_hostname=host)
+            s.connect((host, port))
 
-    # define the sensor data
-    temperature = get_temperature()
-    sensor_data = {
-        "sensor_type": sensor_type,
-        "sensor_name": "temperature_sensor",
-        "sensor_value": temperature,
-        "sensor_unit": "Celsius",
-        "sensor_id": "xyz987" # new: add sensor id
-    }
+            # define the sensor data
+            temperature = get_temperature()
+            humidity = getHumidity()
+            tempHumidSensor_data = {
+                "sensor_type": sensor_type,
+                "sensor_name": "temperatureHumidity_sensor",
+                "sensor_tempValue": temperature,
+                "sensor_humidityValue": humidity,
+                "sensor_unit": "Celsius and %",
+                "sensor_id": "321cba" # new: add sensor id
+            }
 
-    # convert the sensor data to JSON format
-    sensor_json = json.dumps(sensor_data)
+            # convert the sensor data to JSON format
+            sensor_json = json.dumps(tempHumidSensor_data)
 
-    # set up the encryption key and IV
-    key = b'mysecretpassword'
-    iv = key
-
-    # ensure the IV is 16 bytes long
-    if len(iv) != 16:
-        raise ValueError("IV must be 16 bytes long")
-
-    # create the AES cipher object
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-
-    # pad the sensor data to a multiple of 16 bytes
-    padded_data = pad(sensor_json.encode(), AES.block_size)
-
-    # encrypt the padded sensor data using AES-CBC
-    encrypted_data = cipher.encrypt(padded_data)
-
-    # send the encrypted sensor data to the server
-    s.sendall(encrypted_data)
-
-    # receive the response from the server
-    encrypted_response = s.recv(1024)
-    print("Received encrypted server response: ", encrypted_response)
+            # set up the encryption key and IV
+            key = b'mysecretpassword'
+            iv = key
     
-    # create a new AES cipher object for decrypting the response data
-    decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+            # ensure the IV is 16 bytes long
+            if len(iv) != 16:
+                raise ValueError("IV must be 16 bytes long")
 
-    # decrypt the response using AES-CBC
-    decrypted_response = decrypt_cipher.decrypt(encrypted_response)
+            # create the AES cipher object
+            cipher = AES.new(key, AES.MODE_CBC, iv)
 
-    # unpad the decrypted response
-    unpadded_response = unpad(decrypted_response, AES.block_size)
+            # pad the sensor data to a multiple of 16 bytes
+            padded_data = pad(sensor_json.encode(), AES.block_size)
+
+            # encrypt the padded sensor data using AES-CBC
+            encrypted_data = cipher.encrypt(padded_data)
+
+            # send the encrypted sensor data to the server
+            s.sendall(encrypted_data)
+
+            # receive the response from the server
+            encrypted_response = s.recv(1024)
+            print("Received encrypted server response: ", encrypted_response)
     
-    # convert the response from bytes to string format
-    response = unpadded_response.decode()
+            # create a new AES cipher object for decrypting the response data
+            decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+    
+            # decrypt the response using AES-CBC
+            decrypted_response = decrypt_cipher.decrypt(encrypted_response)
 
-    print("Decrypted Response from server: ", response)
+            # unpad the decrypted response
+            unpadded_response = unpad(decrypted_response, AES.block_size)
+        
+            # convert the response from bytes to string format
+            response = unpadded_response.decode()
+    
+            print("Decrypted Response from server: ", response)
+    
+            # close the connection
+            s.close()
+       
+            # Interval for the update
+            time.sleep(5)
 
-    # close the connection
-    s.close()
+        except socket.error as e:
+            print(f"Socket error occurred: {e}")
 
 run_client()
 
